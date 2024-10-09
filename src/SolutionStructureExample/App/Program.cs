c77558fd15d32b51577a9eb83d457d53c5cd0a5d
Radek Pelikán <pelikan@spsmb.cz>
2024-10-09 14:57:44 +0200

    FileLibrary

--- src/SolutionStructureExample/App/Program.cs
+++ src/SolutionStructureExample/App/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using ConsoleLibrary;
+using FileLibrary;
 
 namespace App
 {
@@ -8,18 +10,12 @@ namespace App
     {
         static void Main(string[] args)
         {
+            string path = "./res/movies.csv";
 
-            Grid grid = new Grid(7);
-
-            Point p1 = new Point(1, 6);
-            Point p2 = new Point(2, 7);
-            Point p3 = new Point(5, 2);
-            Point p4 = new Point(3, 1);
-            grid.AddPoint(p1);
-            grid.AddPoint(p2);
-            grid.AddPoint(p3);
-            grid.AddPoint(p4);
-            grid.Print();
+            CsvParser parser = new CsvParser(path);
+            
+            string text = parser.ReadFile();
+            Console.WriteLine(text);
         }
     }
 
---
+++ src/SolutionStructureExample/FileLibrary/CsvParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileLibrary
+{
+    public class CsvParser
+    {
+        public CsvParser(string path)
+        {
+            this.path = path;
+        }
+
+        private string path = "./res/movies.csv";
+
+        public string ReadFile()
+        {
+            string text = "";
+            string line;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    text += line + '\n';
+                    
+                }
+            }
+            return text;
+        }
+    }
+}
