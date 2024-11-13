75eed72123bd5f96e75213462f9ce4eae79a5caf
Radek Pelikán <pelikan@spsmb.cz>
2024-11-13 11:24:43 +0100

    Fileparser

---
+++ src/FileParser/FileParser/Movie.cs
@@ -0,0 +1,23 @@
+﻿class Movie
+{
+    public string Film;
+    public string Genre;
+    public string LeadStudio;
+    public double AudienceScore;
+    public int Profitability;
+    public int rating;
+    public int WorldwideGross;
+    public int Year;
+
+    public Movie(string film, string genre, string leadStudio, double audienceScore, int profitability, int rating, int worldwideGross, int year)
+    {
+        Film = film;
+        Genre = genre;
+        LeadStudio = leadStudio;
+        AudienceScore = audienceScore;
+        Profitability = profitability;
+        this.rating = rating;
+        WorldwideGross = worldwideGross;
+        Year = year;
+    }
+}
---
+++ src/FileParser/FileParser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO; //℃
+
+try
+{
+
+    var lines = File.ReadLines("movies.csv");
+    Console.WriteLine(lines);
+    List<Movie> jenda = new List<Movie>();
+    
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+        jenda.Add(ParseMovie(line));
+    }
+    Console.WriteLine(jenda.Count);
+    Console.ReadLine();
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.ReadLine();
+
+    Environment.Exit(1);
+}
+
+// ༼ つ ◕_◕ ༽つ
+ Movie ParseMovie(string movieLine)
+ {
+     
+     var words = movieLine.Split(",");
+    
+
+     return new Movie(words[0],
+         words[1],
+         words[2],
+         Double.Parse(words[3]),
+         Int32.Parse(words[4]),
+         Int32.Parse(words[5]),
+         Int32.Parse(words[6]),
+         Int32.Parse(words[7]));
+ }
