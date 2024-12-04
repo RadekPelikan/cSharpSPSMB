9dbbb8fe2e2bb3a3820287d48868674a748fe21e
Radek Pelikán <pelikan.radek03@gmail.com>
2024-12-04 10:16:24 +0100

    Identital a IEquatable

---
+++ src/ObjectOverrides/ObjectOverrides/Person.cs
@@ -0,0 +1,35 @@
+﻿public class Person : IEquatable<Person>
+{
+    public string Name;
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+    
+    public bool Equals(Person? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Name == other.Name;
+    }
+    
+    public static bool operator ==(Person obj1, Person obj2)
+    {
+        
+        if (ReferenceEquals(obj1, obj2)) 
+            return true;
+        if (ReferenceEquals(obj1, null)) 
+            return false;
+        if (ReferenceEquals(obj2, null))
+            return false;
+        return obj1.Equals(obj2);
+    }
+    
+    public static bool operator !=(Person obj1, Person obj2)
+    {
+        return !(obj1 == obj2);
+    }
+}
+
+
---
+++ src/ObjectOverrides/ObjectOverrides/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var pepa1 = new Person("Pepa Novak");
+var pepa2 = new Person("Pepa Novak");
+var filip = new Person("Filip Novotny");
+
+if (pepa1 == pepa2)
+{
+    Console.WriteLine("Jsou to stejni bludi");
+}
+else
+{
+    Console.WriteLine("Nejsme stejni");
+}
