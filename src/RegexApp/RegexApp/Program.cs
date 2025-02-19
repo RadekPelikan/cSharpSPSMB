975091a92faf03191424914a32dbdc15ff7f52de
Radek Pelikán <pelikan@spsmb.cz>
2025-02-19 13:34:30 +0100

    regex zadani

---
+++ src/RegexApp/RegexApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Napiste regex: ");
+string regex = Console.ReadLine();
+Console.WriteLine("Napiste string pro testovani regexu: ");
+string value = Console.ReadLine();
+if (true)
+    Console.WriteLine("Regex sedi s hodnotou");
+else
+    Console.WriteLine("Regex nesedi s hodnotou");
+
+
+///// CAST 2
+
+// https://github.com/commandlineparser/commandline
+// pouziti programu: regexApp.exe -r "\d+" -v 3425
+// vyprintuje "Regex sedi", "Regex nesedi"
+
+
+///// CAST 3
+
+// regexApp.exe regexValues.txt -o results.txt
+// regexValues.txt obashuje:
+// prvni radek: (regex) | pod nim dalsi radky hodonty pro otestovani
+
+// \d+
+// 4546
+// 45 fgh
+// 5436
+
+// vystupni soubor:
+// 1: sedi
+// 2: nesedi
+// 3: sedi
