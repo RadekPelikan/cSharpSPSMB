501822cb9bfc9e0121ee22a366cd91a06d2567fa
Radek Pelikán <pelikan@spsmb.cz>
2025-03-05 12:20:27 +0100

    DB web app

---
+++ src/RPGApp/RPGApp/Controllers/EnemyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPGApp.DAL;
+
+namespace RPGApp.Controllers;
+
+public class EnemyController : Controller
+{
+    private readonly ILogger<EnemyController> _logger;
+
+    public EnemyController(ILogger<EnemyController> logger)
+    {
+        _logger = logger;
+    }
+}
