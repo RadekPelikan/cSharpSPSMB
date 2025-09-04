7a9ce6efb655fd2512536c0fcbcc1d2f840ea5c0
ondrej.tomsicek <ondrej.tomsicek@student.spsmb.cz>
2025-09-04 11:19:16 +0200

    sko

--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -4,7 +4,7 @@ namespace setup
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             
             for (int i = 0; i < 100; i++)
@@ -26,6 +26,43 @@ namespace setup
                 }
                 
             }
+        }*/
+        //hledání cisla
+        public static void Main(string[] args)
+        {
+            int max;
+            int min;
+            int pokusy;
+            bool jo = true;
+            Console.WriteLine("zadej spodní hranici");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("zadej horní hranici");
+            max = Convert.ToInt32(Console.ReadLine());
+            while (jo == true)
+            {
+                Console.WriteLine("napis cislo");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number > max)
+                {
+                    pokusy++;
+                    Console.WriteLine("hledane cislo je mensi");
+                }
+                else if (number < min)
+                {
+                    Console.WriteLine("hledane cislo je vetsi");
+                }
+                else
+                {
+                    Console.WriteLine("vyhral si");
+                    jo = false;
+                }
+            }
+
+            
+
+
+
         }
+
     }
 }
