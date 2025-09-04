280c6a584c61107c3a2b0697d16b6fd55dd7ad39
Petr Kredba <petr.kredba@student.spsmb.cz>
2025-09-04 11:01:41 +0200

    penis

--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -1,9 +1,19 @@
-namespace setup
+using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        public static void Main(string[] args)
+        for (int i = 1; i <= 100; i++)
         {
+            if (i % 15 == 0)
+                Console.WriteLine("FizzBuzz");
+            else if (i % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (i % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(i);
         }
     }
 }
