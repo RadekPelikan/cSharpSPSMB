4bd071a41ee7395fa72fc27e6df53711538d28d3
ondrej.tomsicek <ondrej.tomsicek@student.spsmb.cz>
2025-09-04 11:01:54 +0200

    jo

--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -1,9 +1,31 @@
+using System;
+
 namespace setup
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if  (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
         }
     }
 }
