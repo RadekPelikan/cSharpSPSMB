67048d878cfd46da215cde77d68920ca45b06276
Michal Dvořák <michal.dvorak@student.spsmb.cz>
2025-09-11 15:20:27 +0200

    filmy

---
+++ src/4rocnik/Maturita/Files/Movie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Files
+{
+    public class Movie
+    {
+        //Film,Genre,Lead Studio,Audience score %,Profitability,Rotten Tomatoes %,Worldwide Gross,Year
+        
+        public string FilmName { get; set; }
+        public string Genre { get; set; }
+        public string LeadStudio { get; set; }
+        public string AudienceScore { get; set; }
+        public decimal Profitability { get; set; }
+        public int RottenTomatoes { get; set; }
+        public decimal WorldwideGross { get; set; }
+        public int Year { get; set; }
+
+        public Movie(string line)
+        {
+            var values = line.Split(',');
+            FilmName = values[0];
+            Genre = values[1];
+            LeadStudio = values[2];
+            AudienceScore = values[3];
+            Profitability = parseDecimal(values[4]);
+            RottenTomatoes = parseNumber(values[5]);
+            WorldwideGross = parseGroossIncome(values[6]);
+            Year = parseNumber(values[7]);
+        }
+        
+        public int parseNumber(string number)
+        {
+            number = number.Trim();
+            bool isNumber = int.TryParse(number, out int result);
+            if (!isNumber) return 0;
+            return result;
+        }
+        public decimal parseDecimal(string number)
+        {
+            number = number.Trim();
+            bool isNumber = decimal.TryParse(number, NumberStyles.AllowDecimalPoint, CultureInfo.GetCultureInfo("en-Us"), out decimal result);
+            if (!isNumber) return 0;
+            return result;
+        }
+        public decimal parseGroossIncome(string number)
+        {
+            string onlyNumber = number.Substring(1);
+            decimal parsedNum = parseDecimal(onlyNumber);
+            return parsedNum;
+        }
+        
+    }
+}
--- src/4rocnik/Maturita/Files/Program.cs
+++ src/4rocnik/Maturita/Files/Program.cs
@@ -1,9 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Files
 {
   internal class Program
   {
     public static void Main(string[] args)
     {
+       List<Movie> AllMovies = GetAllMovies();
+
+       int yearOftheOldestMovie = 2025;
+       int yearOfTheNewestMovie = 0;
+       
+       foreach (var movie in AllMovies)
+       {
+         if (movie.Year < yearOftheOldestMovie)
+         {
+           yearOftheOldestMovie = movie.Year;
+         }
+         if (movie.Year > yearOfTheNewestMovie)
+         {
+           yearOfTheNewestMovie = movie.Year;
+         }
+       }
+
+       Console.WriteLine(yearOfTheNewestMovie);
+       Console.WriteLine(yearOftheOldestMovie);
+
+       for (int year = yearOftheOldestMovie; year <= yearOfTheNewestMovie; year++)
+       {
+         Console.WriteLine($"-------------------YEAR {year}------------------------");
+         Console.WriteLine(LowestProfitability(AllMovies, year).FilmName);
+         Console.WriteLine(HighestProfitability(AllMovies, year).FilmName);
+         Console.WriteLine(LowestRated(AllMovies, year).FilmName);
+         Console.WriteLine(HighestRated(AllMovies, year).FilmName);
+         Console.WriteLine(AverageGross(AllMovies, year));
+         Console.WriteLine($"----------------------------------------------------");
+         Console.WriteLine();
+       }
+       
+       Console.WriteLine(YearMedian(AllMovies));
+       
+       
+    }
+
+    public static List<Movie> GetAllMovies()
+    {
+      List<Movie> AllMovies = new List<Movie>();
+      using(var reader = new StreamReader(@"./movies.csv"))
+      {
+        reader.ReadLine();
+        while (!reader.EndOfStream)
+        {
+          var line = reader.ReadLine();
+          Movie movie = new Movie(line);
+          AllMovies.Add(movie);
+          //Console.WriteLine(movie);
+        }
+      }
+      return AllMovies;
+    }
+
+    public static Movie LowestProfitability(List<Movie> AllMovies, int year)
+    {
+      
+      decimal lowestProfibalitiy = int.MaxValue;
+      int index = 0;
+      for (int i = 0; i < AllMovies.Count; i++)
+      {
+        if(AllMovies[i].Year != year) continue;
+        if (AllMovies[i].Profitability >= lowestProfibalitiy) continue;
+        lowestProfibalitiy = AllMovies[i].Profitability;
+        index = i;
+      }
+      return AllMovies[index];
+      
+    }
+    
+    public static Movie HighestProfitability(List<Movie> AllMovies, int year)
+    {
+      decimal highestProfitability = 0;
+      int index = 0;
+      for (int i = 0; i < AllMovies.Count; i++)
+      {
+        if(AllMovies[i].Year != year) continue;
+        if (AllMovies[i].Profitability <= highestProfitability) continue;
+        highestProfitability = AllMovies[i].Profitability;
+        index = i;
+      }
+      return AllMovies[index];
+    }
+    
+    public static Movie LowestRated(List<Movie> AllMovies, int year)
+    {
+      
+      int lowestRated = int.MaxValue;
+      int index = 0;
+      for (int i = 0; i < AllMovies.Count; i++)
+      {
+        if(AllMovies[i].Year != year) continue;
+        if (AllMovies[i].RottenTomatoes >= lowestRated) continue;
+        lowestRated = AllMovies[i].RottenTomatoes;
+        index = i;
+      }
+      return AllMovies[index];
+    }
+    
+    public static Movie HighestRated(List<Movie> AllMovies, int year)
+    {
+      
+      int highestRated = 0;
+      int index = 0;
+      for (int i = 0; i < AllMovies.Count; i++)
+      {
+        if(AllMovies[i].Year != year) continue;
+        if (AllMovies[i].RottenTomatoes <= highestRated) continue;
+        highestRated = AllMovies[i].RottenTomatoes;
+        index = i;
+      }
+      return AllMovies[index];
+    }
+
+    public static decimal AverageGross(List<Movie> AllMovies, int year)
+    {
+      decimal sum = 0;
+      foreach (var movie in AllMovies)
+      {
+        if(movie.Year != year) continue;
+        sum += movie.WorldwideGross;
+      }
+      return sum / AllMovies.Count;
+    }
+
+    public static int YearMedian(List<Movie> AllMovies)
+    {
+      return AllMovies[AllMovies.Count / 2].Year;
     }
   }
 }
