0b2163f22c786adc39e71cf0c5a3e6014b9a1e01
ondrej.tomsicek <ondrej.tomsicek@student.spsmb.cz>
2025-09-11 11:16:42 +0200

    penmis

--- src/4rocnik/Maturita/Files/Program.cs
+++ src/4rocnik/Maturita/Files/Program.cs
@@ -35,6 +35,38 @@ class Program
             movies.Add(new Movies(film, genere, studio, audienceScore, profit, rottenTomatoesScore, worldWideGross,
                 year));
         }
+        var byYear = movies.GroupBy(m => m.Year);
+
+        foreach (var group in byYear)
+        {
+            int year = group.Key;
+
+            var worstProfit = group.OrderBy(m => m.profit).First();
+            var bestProfit = group.OrderByDescending(m => m.Profitability).First();
+            var topRotten = group.OrderByDescending(m => m.RottenTomatoesScore).First();
+            var lowRotten = group.OrderBy(m => m.RottenTomatoesScore).First();
+            var avgGross = group.Average(m => m.WorldwideGross);
+
+            Console.WriteLine($"Rok {year}:");
+            Console.WriteLine($"   Nejhorší profitability: {worstProfit.Film} ({worstProfit.Profitability})");
+            Console.WriteLine($"   Nejlepší profitability: {bestProfit.Film} ({bestProfit.Profitability})");
+            Console.WriteLine($"   Nejvíc Rotten Tomatoes %: {topRotten.Film} ({topRotten.RottenTomatoesScore}%)");
+            Console.WriteLine($"   Nej míň Rotten Tomatoes %: {lowRotten.Film} ({lowRotten.RottenTomatoesScore}%)");
+            Console.WriteLine($"   Průměr Worldwide Gross: {avgGross:N2}");
+            Console.WriteLine();
+        }
+
+        // --- medián roků ---
+        var years = movies.Select(m => m.year).Distinct().OrderBy(y => y).ToList();
+        double median;
+        int count = years.Count;
+
+        if (count % 2 == 1)
+            median = years[count / 2];
+        else
+            median = (years[count / 2 - 1] + years[count / 2]) / 2.0;
+
+        Console.WriteLine($"Medián roků: {median}");
 
        
     }
