29a0fb157a01c153b1cf9bbce2e895b6678d1d88
radek.pelikan <pelikan.radek03@gmail.com>
2025-09-18 15:09:34 +0200

    program.cs

--- src/4rocnik/Maturita/OopExamples/Program.cs
+++ src/4rocnik/Maturita/OopExamples/Program.cs
@@ -1,11 +1,1 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
-Person person = new Person("Pepa");
-person = null;
-
-Console.WriteLine(person);
-
-class Person(string Name);
-
-
