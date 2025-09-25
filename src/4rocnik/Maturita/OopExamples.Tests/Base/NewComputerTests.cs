ecfc4f4b51a1f59660f45268fca8c1330516607e
pelikan <pelikan.radek03@gmail.com>
2025-09-25 11:10:09 +0200

    testy

--- src/4rocnik/Maturita/OopExamples.Tests/Base/NewComputerTests.cs
+++ src/4rocnik/Maturita/OopExamples.Tests/Base/NewComputerTests.cs
@@ -4,13 +4,17 @@ namespace OopExamples.Tests;
 
 public class NewComputerTests
 {
+    protected readonly IComputerConfiguration ComputerConfiguration;
     protected readonly IComputer Computer;
+    protected readonly IComputerBuilder Builder;
     protected readonly IPerson Person;
     protected readonly ICompany Company;
 
     public NewComputerTests()
     {
         // Create instance of interfaces, using your implementation
+        ComputerConfiguration = null;
+        Builder = null;
         Computer = null;
         Person = null;
         Company = null;
@@ -19,5 +23,14 @@ public class NewComputerTests
         Computer = Computer ?? throw new System.NotImplementedException($"{nameof(Computer)} not implemented");
         Person = Person ?? throw new System.NotImplementedException($"{nameof(Person)} not implemented");
         Company = Company ?? throw new System.NotImplementedException($"{nameof(Company)} not implemented");
+    }
+
+    protected bool IsValidComputer(IComputer computer)
+    {
+        Assert.NotNull(Computer.MotherBoard);
+        Assert.NotNull(Computer.Gpu);
+        Assert.NotNull(Computer.Ram);
+        Assert.NotNull(Computer.PowerSupply);
+        Assert.NotNull(Computer.Case);
     }
 }
---
+++ src/4rocnik/Maturita/OopExamples.Tests/NewComputerTests/Compute.cs
@@ -0,0 +1,109 @@
+﻿using OopExamples.Interfaces.Exceptions;
+
+namespace OopExamples.Tests;
+
+public class BuildNewComputer : NewComputerTests
+{
+    [Fact]
+    public void ValidComputer_IsValid()
+    {
+        var computer = Builder
+            .AddMotherBoard(ComputerConfiguration.MotherBoard)
+            .AddCPU(ComputerConfiguration.Cpu)
+            .AddGPU(ComputerConfiguration.Gpu)
+            .AddRam(ComputerConfiguration.Ram)
+            .AddPowerSupply(ComputerConfiguration.PowerSupply)
+            .AddCase(ComputerConfiguration.Case)
+            .Build();
+        IsValidComputer(computer);
+    }
+
+    [Fact]
+    public void ComputerWithNothing_ThrowsMissingComponents()
+    {
+        Assert.Throws<ComputerMissingComponentsException>(() =>
+        {
+            var computer = Builder
+                .Build();
+        });
+    }
+
+    [Fact]
+    public void ComputerWithOnlyCpu_ThrowsMissingComponents()
+    {
+        Assert.Throws<ComputerMissingComponentsException>(() =>
+        {
+            var computer = Builder
+                .AddCPU(ComputerConfiguration.Cpu)
+                .Build();
+        });
+    }
+
+    [Fact]
+    public void ComputerWithOnlyGpu_ThrowsMissingComponents()
+    {
+        Assert.Throws<ComputerMissingComponentsException>(() =>
+        {
+            var computer = Builder
+                .AddGPU(ComputerConfiguration.Gpu)
+                .Build();
+        });
+    }
+
+    [Fact]
+    public void ComputerWithOnlyRam_ThrowsMissingComponents()
+    {
+        Assert.Throws<ComputerMissingComponentsException>(() =>
+        {
+            var computer = Builder
+                .AddRam(ComputerConfiguration.Ram)
+                .Build();
+        });
+    }
+
+    [Fact]
+    public void ComputerWithOnlyPowerSupply_ThrowsMissingComponents()
+    {
+        Assert.Throws<ComputerMissingComponentsException>(() =>
+        {
+            var computer = Builder
+                .AddPowerSupply(ComputerConfiguration.PowerSupply)
+                .Build();
+        });
+    }
+
+    [Fact]
+    public void ComputerWithOnlyCase_ThrowsMissingComponents()
+    {
+        Assert.Throws<ComputerMissingComponentsException>(() =>
+        {
+            var computer = Builder
+                .AddCase(ComputerConfiguration.Case)
+                .Build();
+        });
+    }
+}
+
+public class Compute : NewComputerTests
+{
+    [Theory]
+    [InlineData("4 + 5", 9)]
+    [InlineData("4 - 8", -4)]
+    [InlineData("9 *   8 ", 72)]
+    [InlineData(" 9 / 2", 4.5)]
+    public void ComputeEquations_Equal(string equation, float result)
+    {
+        Assert.Equal(Computer.Compute(equation), result);
+    }
+
+    [Theory]
+    [InlineData("4 + 5", 45)]
+    [InlineData("4 - 8", 8)]
+    [InlineData("  9 *   8 ", 4)]
+    [InlineData(" 9 / 2", 5)]
+    [InlineData(" 9 / 2", 4)]
+    public void ComputeEquations_NotEqual(string equation, float result)
+    {
+        Assert.NotEqual(Computer.Compute(equation), result);
+    }
+}
