e003ac37bf040cb0bd7ab36428b48fd99b4c3cdf
pelikan <pelikan.radek03@gmail.com>
2025-09-25 11:12:09 +0200

    program update

--- src/4rocnik/Maturita/OopExamples/Program.cs
+++ src/4rocnik/Maturita/OopExamples/Program.cs
@@ -1,16 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-using OopExamples.Interfaces;
-
-IComputerBuilder builder;
-
-var computer = builder
-    .AddCase(null)
-    .AddCPU(null)
-    .AddGPU(null)
-    .AddCPU(null)
-    .AddMotherBoard(null)
-    .AddPowerSupply(null)
-    .AddRam(null)
-    .Build();
+Console.WriteLine("Hello");
