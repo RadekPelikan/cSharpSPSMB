b351310bd714b651e01092c8c5750fffdf182818
Radek Pelikan <pelikan.radek03@gmail.com>
2026-01-29 13:34:48 +0100

    ef core

---
+++ src/4rocnik/EFCoreVirgin/EFCoreVIrgin.Data.EF/Entity/StudentEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreVIrgin.Data.EF.Entity;
+
+[Table("Student")]
+public class StudentEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public ProfileEntity Profile { get; set; }
+}
