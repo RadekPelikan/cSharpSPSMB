4ccfb9f3314d3e2144cbf44f38d35e22e666cc2c
pelikan <pelikan.radek03@gmail.com>
2026-02-12 09:35:43 +0100

    advent of code

---
+++ src/4rocnik/Maturita/AdventOfCode/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using var fileStream = File.OpenText("input.txt");
+
+int counter = 0;
+int safeValue = 50;
+while (fileStream.Peek() != 0)
+{
+    var line = fileStream.ReadLine();
+    if (line is null)
+        break;
+    Console.WriteLine(line);
+    var direction = line[0];
+    if (int.TryParse(line.Remove(0, 1), out var value) is false)
+    {
+        Console.WriteLine("Error happened during parsing number");
+    }
+
+    var diff = direction switch
+    {
+        'L' => -value,
+        'R' => value,
+    };
+
+    if (Math.Abs(safeValue + diff) > 99)
+    {
+        ++counter;
+    }
+
+    safeValue = (safeValue + diff) % 100;
+
+    if (safeValue == 0)
+    {
+        ++counter;
+    }
+}
+
+Console.WriteLine($"Part1: {counter}");
--- src/4rocnik/Maturita/Zkouseni/Program.cs
+++ src/4rocnik/Maturita/Zkouseni/Program.cs
@@ -1,7 +1,22 @@
-﻿
-
-using MySqlConnector;
-using Zkouseni;
+﻿using Zkouseni;
 
 Console.WriteLine("Hello");
+
+// stahni balicek mysqlconnector hotovo
+// precti si heslo
+Helpers.ReadSecret();
+
+
+string ServerDomain = "vydb1.spsmb.cz";
+string Username = "radek.pelikan";
+string Password = "heslo123";
+string Database = "student_radek.pelikan_duolingo";
+
+string connectionString = $"Server={ServerDomain};Database={Database};User={Username};Password={Password};SslMode=None;";
+MySqlConnector.MySqlConnection connection = new MySqlConnector.MySqlConnection(connectionString);
+connection.Open();
+MySqlConnector.MySqlCommand command = new MySqlConnector.MySqlCommand("SELECT * FROM users" ,connection);
+
+// ziskej nejake zaznamy ze sve databaze. Uloz je do recordu
+// vypis recordy
 
