4c440d894acf6f82486fcd798b13dd18e422ae1b
matej.silhan <matej.silhan@student.spsmb.cz>
2026-02-12 11:20:14 +0100

    aa

--- src/4rocnik/EFCoreVirgin/EFCoreVIrgin.Data.EF/Repository/TeacherRepository.cs
+++ src/4rocnik/EFCoreVirgin/EFCoreVIrgin.Data.EF/Repository/TeacherRepository.cs
@@ -1,31 +1,53 @@
+using EFCoreVIrgin.Data.EF.Context;
 using EFCoreVIrgin.Data.EF.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreVirgin.Common.Repository;
 
 public class TeacherRepository : IBaseRepository<TeacherEntity>
 {
+    private readonly AppDbContext _dbContetx;
+
+    public TeacherRepository(AppDbContext dbContext)
+    {
+        _dbContetx = dbContext;
+    }
+
+   
     public TeacherEntity GetById(int id)
     {
-        throw new NotImplementedException();
+        return _dbContetx.Teachers
+            .Include(t => t.TimeTableRecords)
+                                    .FirstOrDefault(t => t.Id == id);
     }
 
     public List<TeacherEntity> GetAll()
     {
-        throw new NotImplementedException();
+        return _dbContetx.Teachers.ToList();
     }
 
     public TeacherEntity Add(TeacherEntity entity)
     {
-        throw new NotImplementedException();
+        _dbContetx.Teachers.Add(entity);
+        _dbContetx.SaveChanges();
+        return entity;
     }
 
     public TeacherEntity Update(TeacherEntity entity)
     {
-        throw new NotImplementedException();
+        _dbContetx.Teachers.Update(entity);
+        _dbContetx.SaveChanges();
+
+        return entity;
     }
 
     public TeacherEntity Remove(int id)
     {
-        throw new NotImplementedException();
+        var teacher = _dbContetx.Teachers
+            .Include(t => t.TimeTableRecords)
+            .FirstOrDefault(t => t.Id == id);
+        _dbContetx.Teachers.Remove(teacher);
+        _dbContetx.SaveChanges();
+        return teacher;
     }
 }
