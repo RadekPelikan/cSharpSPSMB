b72a90d464297808d4d8826cbb1bd5bc4f17bf4a
ondrej.tomsicek <ondrej.tomsicek@student.spsmb.cz>
2026-02-12 10:49:25 +0100

    Studnet Repo done snad

--- src/4rocnik/EFCoreVirgin/EFCoreVIrgin.Data.EF/Repository/StudentRepository.cs
+++ src/4rocnik/EFCoreVirgin/EFCoreVIrgin.Data.EF/Repository/StudentRepository.cs
@@ -1,31 +1,60 @@
+using EFCoreVIrgin.Data.EF.Context;
 using EFCoreVIrgin.Data.EF.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreVirgin.Common.Repository;
 
 public class StudentRepository : IBaseRepository<StudentEntity>
 {
-    public StudentEntity GetById(int id)
+    private readonly AppDbContext _dbContext;
+
+    public StudentRepository(AppDbContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
     }
 
-    public StudentEntity GetAll()
+    public StudentEntity GetById(int id)
     {
-        throw new NotImplementedException();
+        return _dbContext.Students
+            .Include(s => s.Profile)
+            .Include(s => s.Class)
+            .FirstOrDefault(s => s.Id == id);
+    }
+
+    public List<StudentEntity> GetAll()
+    {
+       return _dbContext.Students
+       .Include(s => s.Profile)
+       .Include(s => s.Class).ToList();
     }
 
     public StudentEntity Add(StudentEntity entity)
     {
-        throw new NotImplementedException();
+        _dbContext.Students.Add(entity);
+        _dbContext.SaveChanges();
+        return entity;
     }
 
     public StudentEntity Update(StudentEntity entity)
     {
-        throw new NotImplementedException();
+        var existingEntity = _dbContext.Students.Find(entity.Id);
+        if (existingEntity != null)
+        {
+            _dbContext.Entry(existingEntity).CurrentValues.SetValues(entity);
+            _dbContext.SaveChanges();
+            return existingEntity;
+        }
+        return null;
     }
 
     public StudentEntity Remove(int id)
     {
-        throw new NotImplementedException();
+        var entity = _dbContext.Students.Find(id);
+        if (entity != null)
+        {
+            _dbContext.Students.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+        return entity;
     }
 }
