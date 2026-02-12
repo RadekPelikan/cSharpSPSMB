d7d9c0fff7c730c21cad21eae559ba3b1a498803
Biggy61 <zdanskystepanek@gmail.com>
2026-02-12 11:04:55 +0100

    facade

--- src/4rocnik/EFCoreVirgin/EFCoreVirgin.Application/Facade/StudentFacade.cs
+++ src/4rocnik/EFCoreVirgin/EFCoreVirgin.Application/Facade/StudentFacade.cs
@@ -1,6 +1,7 @@
 ﻿using EFCoreVirgin.Common.Facade;
 using EFCoreVirgin.Common.Model;
 using EFCoreVirgin.Common.Repository;
+using EFCoreVIrgin.Data.EF.Entity;
 
 namespace EFCoreVirgin.Application.Facade;
 
@@ -8,20 +9,57 @@ public class StudentFacade : IStudentFacade
 {
     // Pouzij repozitare
     private IStudentRepository _StudentRepository { get; init; }
-    
+
     public StudentDetailModel GetById(int id)
     {
-        throw new NotImplementedException();
+        var entity = _StudentRepository.GetById(id);
+
+        if (entity == null) return null; // Or throw exception
+
+        // Step 2: Map Entity -> DetailModel
+        return new StudentDetailModel
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            ClassId = entity.ClassId
+        };
     }
 
     public ListModel<StudentModel> GetAll()
     {
-        throw new NotImplementedException();
+        var entities = _StudentRepository.GetAll();
+
+        // Step 2: Map List<Entity> -> List<StudentModel>
+        var studentModels = entities.Select(e => new StudentModel
+        {
+            Id = e.Id,
+            Name = e.Name,
+            ClassId = e.ClassId
+        }).ToList();
+
+        // Step 3: Wrap in your ListModel
+        return new ListModel<StudentModel>
+        {
+            Items = studentModels
+        };
     }
 
     public StudentDetailModel Create(StudentEditModel editModel)
     {
-        throw new NotImplementedException();
+        
+        var student = _StudentRepository.Add(new StudentEntity()
+        {
+            Name = editModel.Name,
+            ClassId = editModel.ClassId
+        });
+
+
+        return new StudentDetailModel()
+        {
+            Id = student.Id,
+            Name = student.Name,
+            ClassId = student.ClassId
+        };
     }
 
     public StudentDetailModel Update(int id, StudentEditModel editModel)
@@ -31,6 +69,13 @@ public class StudentFacade : IStudentFacade
 
     public StudentDetailModel Delete(int id)
     {
-        throw new NotImplementedException();
+        var student = _StudentRepository.GetById(id);
+
+        return new StudentDetailModel
+        {
+            Id = student.Id,
+            Name = student.Name,
+            ClassId = student.ClassId
+        };
     }
 }
