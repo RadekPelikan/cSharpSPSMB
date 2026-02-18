2fd5ce5611c134720e2f569ab90fc13108c77343
Lukasisnot <lukas.pav0123@gmail.com>
2026-02-19 00:32:24 +0100

    radek zadek

--- src/4rocnik/EFCoreVirgin/EFCoreVirgin.Application/Facade/TimeTableFacade.cs
+++ src/4rocnik/EFCoreVirgin/EFCoreVirgin.Application/Facade/TimeTableFacade.cs
@@ -11,23 +11,56 @@ public class TimeTableFacade : ITimeTableFacade
     private ISubjectRepository SubjectRepository { get; init; }
     private ITeacherRepository TeacherRepository { get; init; }
     private IStudentRepository StudentRepository { get; init; }
-    private ITimeTableRecordRepository TimeTableRecordRepository { get; init; }
     private IClassRepository ClassRepository { get; init; }
+    private ITimeTableRecordRepository TimeTableRecordRepository { get; init; }
 
-    public TimeTableModel GetForClass(int id)
+    private Dictionary<DayOfWeek, TimeTableDayDetailModel> GroupDaysOfCurrentWeekDetail(List<TimeTableRecordEntity> timeTableRecordEntities)
     {
-        var class_ = ClassRepository.GetById(id);
-        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByClassId(id);
-
-
         var now = DateTime.Now;
         var nowDayOfWeek = DateTime.Now.DayOfWeek;
         var startDate = now - new TimeSpan((int)nowDayOfWeek + 1, 0, 0, 0);
         var endDate = now + new TimeSpan((int)(7 - nowDayOfWeek), 0, 0, 0);
 
-        var timeTableRecordsGrouped_ = timeTableRecords_
+        var timeTableRecordsGrouped_ = timeTableRecordEntities
             .Where(e => e.StartTime > startDate && e.StartTime < endDate)
-            .GroupBy(e => e.StartTime.DayOfWeek)
+            .GroupBy(e => e.StartTime.DayOfWeek);
+
+        var timeTableDayModels = new Dictionary<DayOfWeek, TimeTableDayDetailModel>();
+
+        foreach (var timeTableRecordEntitiese in timeTableRecordsGrouped_)
+        {
+            timeTableDayModels.Add(timeTableRecordEntitiese.Key, new TimeTableDayDetailModel()
+            {
+                Day = timeTableRecordEntitiese.Key,
+                ClasesOfTheDay = timeTableRecordEntitiese.Select(c => new TeachingSubjectClassDetailModel()
+                {
+                    StartTime = c.StartTime,
+                    EndTime = c.StartTime + new TimeSpan(0, c.MinuteDuration, 0),
+                    Subject = new SubjectModel()
+                    {
+                        Name = c.Subject.Name,
+                    },
+                    Teacher = new TeacherModel()
+                    {
+                        Name = c.Teacher.Name,
+                    }
+                }).ToList()
+            });
+        }
+
+        return timeTableDayModels;
+    }
+    
+    private Dictionary<DayOfWeek, TimeTableDayModel> GroupDaysOfCurrentWeek(List<TimeTableRecordEntity> timeTableRecordEntities)
+    {
+        var now = DateTime.Now;
+        var nowDayOfWeek = DateTime.Now.DayOfWeek;
+        var startDate = now - new TimeSpan((int)nowDayOfWeek + 1, 0, 0, 0);
+        var endDate = now + new TimeSpan((int)(7 - nowDayOfWeek), 0, 0, 0);
+
+        var timeTableRecordsGrouped_ = timeTableRecordEntities
+            .Where(e => e.StartTime > startDate && e.StartTime < endDate)
+            .GroupBy(e => e.StartTime.DayOfWeek);
 
         var timeTableDayModels = new Dictionary<DayOfWeek, TimeTableDayModel>();
 
@@ -46,34 +79,60 @@ public class TimeTableFacade : ITimeTableFacade
             });
         }
 
+        return timeTableDayModels;
+    }
+
+    public TimeTableModel GetForClass(int id)
+    {
+        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByClassId(id);
         return new TimeTableModel()
         {
-            Days = timeTableDayModels
+            Days = GroupDaysOfCurrentWeek(timeTableRecords_)
         };
     }
 
     public TimeTableModel GetForTeacher(int id)
     {
-        throw new NotImplementedException();
+        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByTeacherId(id);
+        return new TimeTableModel()
+        {
+            Days = GroupDaysOfCurrentWeek(timeTableRecords_)
+        };
     }
 
     public TimeTableModel GetForStudent(int id)
     {
-        throw new NotImplementedException();
+        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByStudentId(id);
+        return new TimeTableModel()
+        {
+            Days = GroupDaysOfCurrentWeek(timeTableRecords_)
+        };
     }
 
     public TimeTableDetailModel GetDetailForClass(int id)
     {
-        throw new NotImplementedException();
+        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByClassId(id);
+        return new TimeTableDetailModel()
+        {
+            Days = GroupDaysOfCurrentWeekDetail(timeTableRecords_)
+        };
     }
 
     public TimeTableDetailModel GetDetailForTeacher(int id)
     {
-        throw new NotImplementedException();
+        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByTeacherId(id);
+        return new TimeTableDetailModel()
+        {
+            Days = GroupDaysOfCurrentWeekDetail(timeTableRecords_)
+        };
     }
 
     public TimeTableDetailModel GetDetailForStudent(int id)
     {
-        throw new NotImplementedException();
+        List<TimeTableRecordEntity> timeTableRecords_ = TimeTableRecordRepository.GetByStudentId(id);
+        return new TimeTableDetailModel()
+        {
+            Days = GroupDaysOfCurrentWeekDetail(timeTableRecords_)
+        };
     }
 }
