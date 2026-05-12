40e339b66279ede21175b76589a5369a8bfe2760
Radek Pelikan <pelikan.radek03@gmail.com>
2026-05-12 19:01:35 +0200

    alphabet otazka 06

--- src/4rocnik/DouckoPRO/otazka06/Program.cs
+++ src/4rocnik/DouckoPRO/otazka06/Program.cs
@@ -5,6 +5,20 @@ Console.WriteLine("Hello, World!");
 var alphabet = "ABCDEFGHIJ";
 // existuje abeceda: ABCDEFGHIJ
 // Vytvořte pole čísel od 0 do 10_000
+
+var word = "HA";
+var i = 0;
+var numbers = new int[10_000].Select(e => i++);
+var words = numbers
+    .Select(n => 
+        string.Join("", 
+            n.ToString().Select((c, i) => alphabet[c - '0'])
+            )
+        )
+    .ToList();
+var count = words.Count(e => e.Contains(word));
+
+Console.WriteLine(count);
 // každé číslo v poli převeďte na abecedu
 // 0 = A
 // 10 = BA
@@ -12,7 +26,6 @@ var alphabet = "ABCDEFGHIJ";
 // 9999 = JJJJ
 // 7070 = HAHA
 
-var word = "HA";
 // LINQ - Select, Where, Count
 // Vyhodnoťte kolikrát se v tomto poli zobrazí slovo "HA"
 
